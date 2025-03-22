d55d9076127242e880e03c82cc0ef68ccd338359
Jesulayomy <jesulayomi081@gmail.com>
2025-03-22 09:52:27 -0400

    [RC]: Even more OOP

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ class Player {
     this.shape = playerShape
     this.wins = playerWins
   }
-  win() {
+  win(numberOfGames) {
     this.wins += 1;
-    document.querySelector('h2').innerText = `${this.shape} WON. ${this.name} has ${this.wins} wins of ${numberOfgames} games`;
+    document.querySelector('h2').innerText = `${this.shape} WON. ${this.name} has ${this.wins} wins of ${numberOfGames} games`;
   }
 }
 
@@ -25,135 +25,116 @@ class Board {
   }
 }
 
-let playerOne = new Player('Player One', '❌', 0);
-let playerTwo = new Player('Player Two', '🟡', 0);
-
-// the game will be played on a grid board of 3 x 3 squares both horizontal and vertical
-// // Create a board of size 3 x 3 = 9
-let board = new Board();
-
-let shapes = {
-  'x': '❌',
-  'o': '🟡',
-  'X': '❌',
-  'O': '🟡',
-}
-
-// declare winner and log games won out of games attempted
-// // Display winner and number of games
-// // Create a games attempted varible to track number of games
-let numberOfgames = 1;
-
-
-document.getElementById('submitNames').addEventListener('click', makePlayers);
-
-function makePlayers() {
-  let playerOneName = document.getElementById('playerOneName').value || playerOne.name;
-  let playerTwoName = document.getElementById('playerTwoName').value || playerTwo.name;
-
-  let playerOneShape = document.querySelector('input[name="playerOneShape"]:checked')?.value || 'x';
-  playerOneShape = shapes[playerOneShape];
-  let playerTwoShape = playerOneShape === shapes['x'] ? shapes['o'] : shapes['x'];
-
-  // // Create two players
-  playerOne.name = playerOneName;
-  playerOne.shape = playerOneShape;
-  playerTwo.name = playerTwoName;
-  playerTwo.shape = playerTwoShape;
-
-  console.log(playerOne);
-  console.log(playerTwo);
-}
-
-
-// players take turns clicking on the open spaces in the grid to lay down their shapes 
-//  // let turn starts as true
-//  // if turn is true, its player 1
-//  // if turn is false its player 2
-// // A function to place shapes on the board
-
-const ticTacToe = document.querySelector('.ticTacToe');
-let turn = true;
-let winning = false;
-
-ticTacToe.addEventListener('click', (e) => {
-  if (winning) {
-    alert('Game Over, please reset the board');
-  } else if (
-    board[e.target.id || e.target.parentElement.id] === playerOne.shape ||
-    board[e.target.id || e.target.parentElement.id] === playerTwo.shape ||
-    winning
-    ) {
-    alert('Play in a different square');
-  } else {
-    if (turn == true) {
-      document.getElementById(`${e.target.id || e.target.parentElement.id}`).innerHTML = `<span>${playerOne.shape}</span>`;
-      board[e.target.id] = playerOne.shape;
-      checkWin();
-    } else {
-      document.getElementById(`${e.target.id || e.target.parentElement.id}`).innerHTML = `<span>${playerTwo.shape}</span>`;
-      board[e.target.id] = playerTwo.shape;
-      checkWin();
+class TicTacToe {
+  constructor() {
+    this.playerOne = new Player('Player One', '❌', 0)
+    this.playerTwo = new Player('Player Two', '🟡', 0)
+    this.board = new Board();
+    this.shapes = {
+      'x': '❌',
+      'o': '🟡',
+      'X': '❌',
+      'O': '🟡',
     }
-    turn = !turn;
+    this.numberOfGames = 1;
+    this.turn = true
+    this.winning = false
   }
-})
+  start() {
+    console.log('GAME START!');
+  }
+  makePlayers() {
+    let playerOneName = document.getElementById('playerOneName').value || this.playerOne.name;
+    let playerTwoName = document.getElementById('playerTwoName').value || this.playerTwo.name;
+  
+    let playerOneShape = document.querySelector('input[name="playerOneShape"]:checked')?.value || 'x';
+    playerOneShape = this.shapes[playerOneShape];
+    let playerTwoShape = playerOneShape === this.shapes['x'] ? this.shapes['o'] : this.shapes['x'];
+  
+    // Create two players
+    this.playerOne.name = playerOneName;
+    this.playerOne.shape = playerOneShape;
+    this.playerTwo.name = playerTwoName;
+    this.playerTwo.shape = playerTwoShape;
 
-
-// when three of the same type are laid, whether horizontally, vertically or diagnally a winner is declared.
-// // A function to determine when a player wins
-function checkWin() {
-  if (board['TC'] == board['TL'] && board['TR'] == board['TC']) {
-    logWin()
-  } else if (board['MC'] == board['ML'] && board['MR'] == board['MC']) {
-    logWin()
-  } else if (board['BC'] == board['BL'] && board['BR'] == board['BC']) {
-    logWin()
-  } else if (board['TC'] == board['MC'] && board['MC'] == board['BC']) {
-    logWin()
-  } else if (board['TL'] == board['ML'] && board['ML'] == board['BL']) {
-    logWin()
-  } else if (board['TR'] == board['MR'] && board['MR'] == board['BR']) {
-    logWin()
-  } else if (board['TL'] == board['MC'] && board['MC'] == board['BR']) {
-    logWin()
-  } else if (board['TR'] == board['MC'] && board['MC'] == board['BL']) {
-    logWin()
+    document.querySelector('h3').innerText = `${this.playerOne.shape} ${this.playerOne.name}  VS  ${this.playerTwo.name} ${this.playerTwo.shape}`
+  }
+  nextGame() {
+    this.board = new Board();
+    this.winning = false;
+    this.numberOfGames += 1;
+    document.getElementById('tic').innerHTML = `
+      <section class="top">
+        <div id="TL"></div>
+        <div id="TC"></div>
+        <div id="TR"></div>
+      </section>
+      <section class="middle">
+        <div id="ML"></div>
+        <div id="MC"></div>
+        <div id="MR"></div>
+      </section>
+      <section class="bottom">
+        <div id="BL"></div>
+        <div id="BC"></div>
+        <div id="BR"></div>
+      </section>
+    `
+  }
+  placeShape(e) {
+    if (this.winning) {
+      alert('Game Over, please reset the board');
+    } else if (
+      this.board[e.target.id || e.target.parentElement.id] === this.playerOne.shape ||
+      this.board[e.target.id || e.target.parentElement.id] === this.playerTwo.shape ||
+      this.winning
+      ) {
+      alert('Play in a different square or reset the board');
+    } else {
+      if (this.turn == true) {
+        document.getElementById(`${e.target.id || e.target.parentElement.id}`).innerHTML = `<span>${this.playerOne.shape}</span>`;
+        this.board[e.target.id] = this.playerOne.shape;
+        this.checkWin();
+      } else {
+        document.getElementById(`${e.target.id || e.target.parentElement.id}`).innerHTML = `<span>${this.playerTwo.shape}</span>`;
+        this.board[e.target.id] = this.playerTwo.shape;
+        this.checkWin();
+      }
+      this.turn = !this.turn;
+    }
+  }
+  checkWin() {
+    if (this.board['TC'] == this.board['TL'] && this.board['TR'] == this.board['TC']) {
+      this.logWin()
+    } else if (this.board['MC'] == this.board['ML'] && this.board['MR'] == this.board['MC']) {
+      this.logWin()
+    } else if (this.board['BC'] == this.board['BL'] && this.board['BR'] == this.board['BC']) {
+      this.logWin()
+    } else if (this.board['TC'] == this.board['MC'] && this.board['MC'] == this.board['BC']) {
+      this.logWin()
+    } else if (this.board['TL'] == this.board['ML'] && this.board['ML'] == this.board['BL']) {
+      this.logWin()
+    } else if (this.board['TR'] == this.board['MR'] && this.board['MR'] == this.board['BR']) {
+      this.logWin()
+    } else if (this.board['TL'] == this.board['MC'] && this.board['MC'] == this.board['BR']) {
+      this.logWin()
+    } else if (this.board['TR'] == this.board['MC'] && this.board['MC'] == this.board['BL']) {
+      this.logWin()
+    }
+  }
+  logWin() {
+    this.winning = true;
+    if (this.turn == true) {
+      this.playerOne.win(this.numberOfGames);
+    } else {
+      this.playerTwo.win(this.numberOfGames);
+    }
   }
 }
 
-// Smaller function to display the win
-function logWin() {
-  winning = true;
-  if (turn == true) {
-    playerOne.win();
-  } else {
-    playerTwo.win();
-  }
-}
 
-// Resete the board
-document.getElementById('nextGame').addEventListener('click', nextGame);
+const game = new TicTacToe();
 
-function nextGame() {
-  board = new Board();
-  winning = false;
-  numberOfgames += 1;
-  document.getElementById('tic').innerHTML = `
-    <section class="top">
-      <div id="TL"></div>
-      <div id="TC"></div>
-      <div id="TR"></div>
-    </section>
-    <section class="middle">
-      <div id="ML"></div>
-      <div id="MC"></div>
-      <div id="MR"></div>
-    </section>
-    <section class="bottom">
-      <div id="BL"></div>
-      <div id="BC"></div>
-      <div id="BR"></div>
-    </section>
-  `
-}
+document.getElementById('submitNames').addEventListener('click', game.makePlayers.bind(game));
+document.getElementById('nextGame').addEventListener('click', game.nextGame.bind(game));
+document.querySelector('.ticTacToe').addEventListener('click', game.placeShape.bind(game))
